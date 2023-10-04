d75121b7dc8ab189c1cef1bd2754e2bf41f53551
Jeremy Gurjar <jeremygurjar10@gmail.com>
2023-10-04 21:02:29 +0100

    commit loopHomework

--- src/loopHomework.java
+++ src/loopHomework.java
@@ -5,7 +5,21 @@ import java.util.Scanner;
 public class loopHomework {
 
     public static void main(String[] args) {
+        Q1();
+        Q2();
+        Q3();
+        Q4();
+        Q5();
         Q6();
+        Q7();
+        Q8();
+        Q9();
+        Q10();
+        Q11();
+        Q12();
+        Q13();
+        Q14();
+        Q15();
     }
 
     static void Q1() {
